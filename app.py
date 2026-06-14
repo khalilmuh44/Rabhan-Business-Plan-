59eb1cc65d190fe64bf86ddaa681c3bf7b5f34c8
khalilmuh44 <khalilmoham14@gmail.com>
2026-06-14 11:13:15 +0300

    Initial commit - Secured project

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ col1, col2, col3 = st.columns([1, 1.5, 1])
 
 with col2:
     
-    st.image("my_img.png", use_container_width=True)
+    st.image("img.png", use_container_width=True)
 st.title("📈 Rabhan Growth Agency")
 st.subheader("AI Media Plan Generator")
 
